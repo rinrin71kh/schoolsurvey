ecc1bc9dd8909566a4c97533aac8306b0c258fa8
khuoy borin satya <khuoyborin@gmail.com>
2025-08-01 14:16:25 +0700

    add dynamic dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,28 @@ export const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('../app/resource/auth/login/component').then(m => m.LoginComponent) },
   {
-    path: 'admin/dashboard',
-    loadComponent: () =>
-      import('./resource/admin/dashboard/component').then((m) => m.DashboardComponent)
+    path: 'admin',
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./resource/admin/dashboard/component').then((m) => m.DashboardComponent)
+      },
+      {
+        path: 'user',
+        loadComponent: () =>
+          import('./resource/admin/user/component').then((m) => m.UserComponent)
+      },
+      {
+        path: 'survey',
+        loadComponent: () =>
+          import('./resource/admin/survey/component').then((m) => m.SurveyComponent)
+      },
+       {
+        path: 'setting',
+        loadComponent: () =>
+          import('./resource/admin/setting/component').then((m) => m.SettingComponent)
+      }
+    ]
   }
 ];
---
+++ src/app/resource/admin/user/component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../component/sidebar/component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CommonModule, SidebarComponent],
+  templateUrl: './template.html',
+  styleUrls: ['./style.css']
+})
+export class UserComponent {}
