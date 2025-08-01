76c655ad24ff74d96f577a04db3441fd96376f38
khuoy borin satya <khuoyborin@gmail.com>
2025-08-01 14:07:51 +0700

    add sidebar

--- src/app/resource/admin/dashboard/component.ts
+++ src/app/resource/admin/dashboard/component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../component/sidebar/component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './template.html',
   styleUrls: ['./style.css']
 })
---
+++ src/app/resource/component/sidebar/component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template.html',
+  styleUrls: ['./style.css']
+})
+export class SidebarComponent {}
