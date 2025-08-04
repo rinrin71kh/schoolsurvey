0deb266f3c28d379f8c67c174ea3d77bbf8acb54
khuoy borin satya <khuoyborin@gmail.com>
2025-08-04 08:46:09 +0700

    improve ui

--- src/app/resource/component/header/component.ts
+++ src/app/resource/component/header/component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'; // <-- import Router
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './template.html',
+  imports : [
+    MatIconModule,
+    CommonModule
+  ],
   styleUrls: ['./style.css']
 })
 export class HeaderComponent {
   isDarkMode = false;
-
+  userMenuOpen = false;
   constructor(private router: Router) {} // <-- inject Router
 
   toggleTheme(): void {
     this.isDarkMode = !this.isDarkMode;
     document.documentElement.classList.toggle('dark', this.isDarkMode);
   }
+  toggleUserMenu(): void {
+    this.userMenuOpen = !this.userMenuOpen;
+  }
 
+  viewProfile(): void {
+    this.router.navigate(['/admin/profile']); // Adjust if you have a different route
+  }
   logout(): void {
     // Navigate to login
     this.router.navigate(['/login']);
--- src/app/resource/component/sidebar/component.ts
+++ src/app/resource/component/sidebar/component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,MatIconModule],
   templateUrl: './template.html',
   styleUrls: ['./style.css']
 })
